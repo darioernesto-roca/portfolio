ea1eba098dcbca1db4fb7450dd69fb2c0cd3f993
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-03-12 00:13:42 -0500

    Updated next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-// Import the next-videos plugin
-const withVideos = require('next-videos');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Any other configurations you have...
 };
 
-// Export the configuration with next-videos plugin applied
-module.exports = withVideos(nextConfig);
+module.exports = nextConfig;
+
+
